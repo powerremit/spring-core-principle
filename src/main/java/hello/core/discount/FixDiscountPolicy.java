175c63f6c4c0d299045924652c26032977517034
powerremit <powerremit@naver.com>
2024-10-24 16:45:00 +0900

    initial commit

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+    private int discountFixAmount = 1000;
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) { // enum type은 == 쓰는게 맞음
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
